a89313ba4ccc09e3cf97dd82003a9f01f6179180
Jonathan Thunberg <thunberg00@gmail.com>
2020-05-21 21:54:18 +0200

    Removed test code in Home.vue

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -9,35 +9,5 @@ import axios from "axios";
 
 export default {
   name: "Home",
-  created() {
-    axios
-      .post(
-        "https://api.vasttrafik.se/token",
-        {
-          headers: {
-            "Authorization": "Basic U3ZFYm80S0pReEZ0dUpMUEExN3JLSllocFJnYTpwRWRmTTNkSWhHblNqa2NQb3JDZE1JVTdOVmdh"
-          }
-        }
-      )
-      .then(response => {
-        console.log(response.data);
-      });
-
-
-      axios({
-        method: 'post',
-        url: baseUrl + 'applications/' + appName + '/dataexport/plantypes' + plan,
-        headers: {
-          "Authorization": "Basic U3ZFYm80S0pReEZ0dUpMUEExN3JLSllocFJnYTpwRWRmTTNkSWhHblNqa2NQb3JDZE1JVTdOVmdh"
-        }, 
-        data: {
-          grant_type: 'client_credentials',
-        }
-      })
-      .then(response => {
-        console.log(response.data);
-      });
-
-  }
 };
 </script>
