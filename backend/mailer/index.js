56db26895406bf3e9932e7c2d6e457b3985be5b7
Jonathan Thunberg <thunberg00@gmail.com>
2020-05-25 13:49:43 +0200

    Removed credentials form mailer index.js file

--- backend/mailer/index.js
+++ backend/mailer/index.js
@@ -1,12 +1,15 @@
 const nodemailer = require('nodemailer');
 
+const credentials = require('./credentials');
+
+
 const transporter = nodemailer.createTransport({
   host: "send.one.com",
   port: 465,
   secure: true,
   auth: {
-    user: 'AutoPlanner@aviliax.com',
-    pass: 'Mhzu@vimk9+z8jm'
+    user: credentials.user,
+    pass: credentials.password
   }
 });
 
