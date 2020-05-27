c2654e3b64d819f21a822357b5cea159258dab10
Mojjan4 <Mojjan4@users.noreply.github.com>
2020-05-27 16:39:21 +0200

    Kan klicka på kalender och event läggs till. Ska modifieras mer så man kan göra det mer personligt.

--- frontend/src/components/Calendar/Calendar.vue
+++ frontend/src/components/Calendar/Calendar.vue
@@ -1,20 +1,59 @@
 <template>
-  <FullCalendar schedulerLicenseKey="XXX" :plugins="calendarPlugins" />
+  <div class="wrapper">
+    <FullCalendar
+      schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
+      :plugins="calendarPlugins"
+      :header="{
+        left: 'prev,next today',
+        center: 'title',
+        right: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek'
+      }"
+      :weekends="calendarWeekends"
+      :events="calendarEvents"
+      @dateClick="handleDateClick"
+    />
+  </div>
 </template>
 
 <script>
 import FullCalendar from "@fullcalendar/vue";
 import resourceTimelinePlugin from "@fullcalendar/resource-timeline";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
 
 export default {
   components: {
     FullCalendar
   },
-  data() {
+  data: function() {
     return {
-      calendarPlugins: [dayGridPlugin]
+      calendarPlugins: [
+        // plugins must be defined in the JS
+        dayGridPlugin,
+        timeGridPlugin,
+        interactionPlugin, // needed for dateClick
+        listPlugin
+      ],
+      calendarWeekends: true,
+      calendarEvents: [
+        // initial event data
+        { title: "Event Now", start: new Date() }
+      ]
     };
+  },
+  methods: {
+    handleDateClick(arg) {
+      if (confirm("Vill du lägga till en resa " + arg.dateStr + " ?")) {
+        this.calendarEvents.push({
+          // add new event data
+          title: "",
+          start: arg.date,
+          allDay: false
+        });
+      }
+    }
   }
 };
 </script>
@@ -23,4 +62,8 @@ export default {
 @import "~@fullcalendar/core/main.css";
 @import "~@fullcalendar/timeline/main.css";
 @import "~@fullcalendar/resource-timeline/main.css";
+@import "~@fullcalendar/timegrid/main.css";
+.wrapper {
+  padding: 50px;
+}
 </style>
