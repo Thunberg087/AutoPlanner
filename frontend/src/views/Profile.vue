0d4c519a66f3dcf6b9b73d5b6d5a78add7483f7c
Jonathan Thunberg <thunberg00@gmail.com>
2020-06-04 11:53:26 +0200

    Search for addresses functionality

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,10 +1,50 @@
 <template>
-  <h1>Hej user!</h1>
+  <div>
+    <h1>Hej user!</h1>
+    <input type="text" list="fetchedLocations" v-model="searchQuery" placeholder="Sök adress">
+    <datalist id="fetchedLocations">
+      <option :value="location.place_name" :key="location.id" v-for="location in fetchedLocations"></option>
+    </datalist>
+  </div>
 </template>
-
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      searchQuery: "",
+      fetchedLocations: []
+    }
+  },
+  watch: {
+    async searchQuery() {
+      
+      if (this.searchQuery === "") {
+        this.fetchedLocations = []
+      } else {
+        let url = `https://api.mapbox.com/geocoding/v5/mapbox.places/`+
+          `${this.searchQuery}.json`+
+          `?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw`+
+          `&autocomplete=true`+
+          `&country=se`+
+          `&types=address`;
+
+        this.axios.get(url)
+          .then(response => {
+            console.log(response);
+            
+            this.fetchedLocations = response.data.features
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      }
+
+ 
+    },
+  }
+};
 </script>
 
 <style scoped>
+
 </style>
