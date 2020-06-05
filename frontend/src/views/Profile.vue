28bdbcf2301d39355daf49b4814aed324a861015
Christian Jäderberg <christianjaderberg@gmail.com>
2020-06-05 11:51:33 +0200

    Cleaned up the styling for a better display and usability

--- frontend/src/views/Profile.vue
+++ frontend/src/views/Profile.vue
@@ -1,17 +1,16 @@
 <template>
-  <div>
-    <h1>Hej user!</h1>
-    <input type="text" list="fetchedLocations" v-model="searchQuery" placeholder="Sök adress">
-    <datalist id="fetchedLocations">
-      <option></option>
-    </datalist>
-    <ul>
-      <li :key="location.id" v-for="location in fetchedLocations" @click="addLocation(location)">{{ location.place_name }}</li>
-    </ul>
-    <h2>Mina sparade locations</h2>
-    <ul>
-      <li :key="i" v-for="(location, i) in userLocations">{{ location.locationName }} <p @click="deleteLocation(location)">TA BORT</p></li>
-    </ul>
+  <div class="main-wrapper">
+    <div class="content">
+      <h1>Hej user!</h1>
+      <input class="search-input" type="text" v-model="searchQuery" placeholder="Sök adress">
+      <ul class="search-list">
+        <li :key="location.id" v-for="location in fetchedLocations">{{ location.place_name }} <button @click="addLocation(location)">+</button></li>
+      </ul>
+      <h2>Mina sparade locations</h2>
+      <ul>
+        <li :key="i" v-for="(location, i) in userLocations">{{ location.locationName }} <button @click="deleteLocation(location)">X</button></li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
@@ -34,7 +33,8 @@ export default {
           `?access_token=pk.eyJ1Ijoic2VhcmNoLW1hY2hpbmUtdXNlci0xIiwiYSI6ImNrN2Y1Nmp4YjB3aG4zZ253YnJoY21kbzkifQ.JM5ZeqwEEm-Tonrk5wOOMw`+
           `&autocomplete=true`+
           `&country=se`+
-          `&types=address`;
+          `&types=address`+
+          `&limit=10`;
 
         this.axios.get(url)
           .then(response => {
@@ -66,10 +66,7 @@ export default {
         });
     },
     addLocation(pickedLocation) {
-
-      console.log("addlocation method: " + pickedLocation.place_name);
-      let url = process.env.VUE_APP_HOST + ":" + process.env.VUE_APP_SERVER_PORT + "/";
-
+      
       let newLocation = {
         userId: this.$store.getters.getUser.id,
         locationName: pickedLocation.place_name,
@@ -77,14 +74,29 @@ export default {
         latitude: pickedLocation.geometry.coordinates[1]
       }
 
-      this.axios
-        .post(url + "usersettings/addLocation", newLocation)
-        .then(res => {
-          this.getLocationList();
-        })
-        .catch(err => {
-          this.errorMessage = err.response.data.msg;
-        });
+      // check if location already exists in list, if so do not add
+      var alreadyAdded = false;
+      for (var i = 0; i < this.userLocations.length; i++) {
+        if (this.userLocations[i].locationName == newLocation.locationName) {
+          alreadyAdded = true;
+        }
+      }
+      if (!alreadyAdded) {
+        console.log("addlocation method: " + pickedLocation.place_name);
+        let url = process.env.VUE_APP_HOST + ":" + process.env.VUE_APP_SERVER_PORT + "/";
+  
+  
+        this.axios
+          .post(url + "usersettings/addLocation", newLocation)
+          .then(res => {
+            this.getLocationList();
+          })
+          .catch(err => {
+            this.errorMessage = err.response.data.msg;
+          });
+      } else {
+        alert("Platsen är redan sparad!")
+      }
     },
     deleteLocation(pickedLocation) {
 
@@ -106,5 +118,40 @@ export default {
 </script>
 
 <style scoped>
-
+  .main-content {
+    display: flex;
+    align-items: center;
+    width: 100%;
+  }
+  .content {
+    text-align: center;
+    width: 500px;
+    margin: 0 auto;
+  }
+  h1, h2 {
+    margin: 10px 0;
+  }
+  ul {
+    list-style-type: none;
+  }
+  .search-input {
+    width: 100%;
+    height: 30px;
+    font-size: 1.2em;
+    border-radius: 15px;
+    border: none;
+    padding: 0 20px;
+  }
+  .search-list {
+    text-align: left;
+    background-color: #a5d9ef;
+    width: 100%;
+    max-height: 130px;
+    overflow-y: scroll;
+    border-bottom-left-radius: 20px;
+    border-bottom-right-radius: 20px; 
+  }
+  .search-list li {
+    margin: 10px 0;
+  }
 </style>
