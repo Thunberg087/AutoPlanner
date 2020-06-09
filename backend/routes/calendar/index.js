c6a03a34b943dc0eada079c95149d2d699c92c73
Jonathan Thunberg <54378565+Thunberg087@users.noreply.github.com>
2020-06-09 13:41:29 +0200

    Feature/#34 (#39)
    * påbörjat visa info om event, databas kan hämta info men inte synligt för användaren än
    
    * kan öppna befintligt event som visar, namn event, start och slut
    
    * testar igen
    
    * rullgardin för sparade platser skall synas sen
    
    Co-authored-by: Mojjan4 <Mojjan4@users.noreply.github.com>

--- backend/routes/calendar/index.js
+++ backend/routes/calendar/index.js
@@ -10,7 +10,7 @@ router.post('/addEvent', (req, res) => {
   console.log(req.body);
 
 
-  if (new Date(req.body.start) > new Date(req.body.end) ) {
+  if (new Date(req.body.start) > new Date(req.body.end)) {
     res.status(406).send({ msg: "Felaktiga datum" })
   }
 
@@ -19,20 +19,20 @@ router.post('/addEvent', (req, res) => {
     `INSERT INTO events (id, userId, title, start, end, allDay) 
     VALUES (${mysqlConnection.escape(uuid.v4())}, ${mysqlConnection.escape(req.body.userId)}, ${mysqlConnection.escape(req.body.title)}, 
     ${mysqlConnection.escape(req.body.start)}, ${mysqlConnection.escape(req.body.end)}, ${mysqlConnection.escape(req.body.allDay)}) `, (err, result) => {
-      if (err) {
-        console.log(err);
-        return res.status(500).send();
-      }
-      return res.status(200).send()
-    });
-  
+    if (err) {
+      console.log(err);
+      return res.status(500).send();
+    }
+    return res.status(200).send()
+  });
+
 })
 
 
 
 router.post('/getEvents', (req, res) => {
   console.log(req.body);
-  
+
   mysqlConnection.query(
     `SELECT * FROM events WHERE userId = ${mysqlConnection.escape(req.body.userId)}`, (err, result) => {
       if (err) {
@@ -41,8 +41,22 @@ router.post('/getEvents', (req, res) => {
       }
       res.status(200).send(result)
     });
-  
+
 })
+
+router.post('/getEvent', (req, res) => {
+  console.log(req.body.id);
+
+  mysqlConnection.query(
+    `SELECT * FROM events WHERE id = ${mysqlConnection.escape(req.body.id)}`, (err, result) => {
+      if (err) {
+        console.log(err);
+        return res.status(500).send();
+      }
+      res.status(200).send(result)
+    });
+
+});
 
 
 module.exports = router;
--- frontend/src/components/Calendar/Calendar.vue
+++ frontend/src/components/Calendar/Calendar.vue
@@ -1,43 +1,71 @@
 <template>
   <div class="wrapper">
     <FullCalendar
-      locale="sv"
-      schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
-      :plugins="calendarPlugins"
-      :header="{
-        left: 'prev, next, today, customButton',
-        center: 'title',
-        right: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek'
-      }"
-      :customButtons="{
-        customButton: {
-          text: 'Lägg till event!',
-          click: openAddEventPopup
-        }
-      }"
-      :aspectRatio="2.8"
-      :weekends="calendarWeekends"
-      :events="calendarEvents"
+    locale="sv"
+    schedulerLicenseKey="GPL-My-Project-Is-Open-Source"
+    :plugins="calendarPlugins"
+    :selectable="true"
+    :header="{
+    left: 'prev, next, today, customButton',
+    center: 'title',
+    right: 'dayGridMonth, timeGridWeek, timeGridDay, listWeek'
+    }"
+    editable: true
+    @eventClick="eventClick"
+    :customButtons="{
+    customButton: {
+    text: 'Lägg till event!',
+    click: openAddEventPopup
+    }
+    }"
+    :aspectRatio="2.8"
+    :weekends="calendarWeekends"
+    :events="calendarEvents"
     />
     <div class="addEventPopup" v-if="statusAddEventPopup">
       <form v-on:submit.prevent="addEvent">
-        <input v-model="eventTitle" type="text" placeholder="Titel" />
+        <input v-model="newEvent.eventTitle" type="text" placeholder="Titel" />
         <div class="checkboxWrapper">
-          <input v-model="eventAllDay" type="checkbox" id="checkbox" />
+          <div class="selectWrapper">
+            <select v-model="selected">
+              <option disabled value>Välj en plats</option>
+              <option>A</option>
+              <option>B</option>
+              <option>C</option>
+            </select>
+          </div>
+          <input v-model="newEvent.eventAllDay" type="checkbox" id="checkbox" />
           <label for="checkbox">Heldag</label>
         </div>
 
         <div class="dateTimeBox">
-          <input v-model="eventStartDate" type="date" />
-          <input v-model="eventStartTime" type="time" v-if="!eventAllDay" />
+          <input v-model="newEvent.eventStartDate" type="date" />
+          <input v-model="newEvent.eventStartTime" type="time" v-if="!newEvent.eventAllDay" />
         </div>
         <div class="dateTimeBox">
-          <input v-model="eventEndDate" type="date" />
-          <input v-model="eventEndTime" type="time" v-if="!eventAllDay" />
+          <input v-model="newEvent.eventEndDate" type="date" />
+          <input v-model="newEvent.eventEndTime" type="time" v-if="!newEvent.eventAllDay" />
         </div>
         <input type="submit" value="Lägg till" />
         <p v-if="errorMessage">{{errorMessage}}</p>
       </form>
+    </div>
+
+    <!-- 
+   - Location ska även visas, eventuellt samt busslinje visas. 
+   - Avisering skall läggas till i med en checbox (diskuteras med gruppen om nödvädnigt) 
+     typ som google kalender, notifikationstabell i databsen. 
+   - Tidsmarginal skall visas och även kunna ändras.
+   - Se busstid? Se bussen som passar bäst för tid man har lagt in?
+    -->
+
+    <div class="showEventInfoPopup" v-if="eventInfo">
+      <div class="showTitleBox">Titel: {{eventInfo.title}}</div>
+
+      <div class="showTimeBox">Start: {{eventInfo.start}}</div>
+      <div class="showTimeBox">Slut: {{eventInfo.end}}</div>
+
+      <button v-on:click="removeEventInfoPopup">Close</button>
     </div>
   </div>
 </template>
@@ -64,35 +92,41 @@ export default {
         listPlugin
       ],
       calendarWeekends: true,
+
       calendarEvents: [
         // initial event data
       ],
+      newEvent: {
+        eventTitle: "",
+        eventStartTime: null,
+        eventEndTime: null,
+        eventStartDate: null,
+        eventEndDate: null,
+        eventAllDay: false
+      },
       errorMessage: null,
       statusAddEventPopup: false,
-      eventTitle: "",
-      eventStartTime: null,
-      eventEndTime: null,
-      eventStartDate: null,
-      eventEndDate: null,
-      eventAllDay: false
+      eventInfo: null
     };
   },
   watch: {
     eventAllDay(newValue) {
       if (newValue == true) {
-        this.eventStartTime = null;
-        this.eventEndTime = null;
+        this.newEvent.eventStartTime = null;
+        this.newEvent.eventEndTime = null;
       }
     }
   },
   created() {
-    let url = process.env.VUE_APP_HOST + ":" + process.env.VUE_APP_SERVER_PORT + "/";
-
+    let url =
+      process.env.VUE_APP_HOST + ":" + process.env.VUE_APP_SERVER_PORT + "/";
 
     this.axios
-      .post(url + "calendar/getEvents", { userId: this.$store.getters.getUser.id })
+      .post(url + "calendar/getEvents", {
+        userId: this.$store.getters.getUser.id
+      })
       .then(res => {
-        this.calendarEvents = res.data
+        this.calendarEvents = res.data;
       })
       .catch(err => {
         this.errorMessage = err.response.data.msg;
@@ -103,24 +137,22 @@ export default {
       this.statusAddEventPopup = true;
     },
     addEvent() {
-      let start = this.eventStartTime
-        ? this.eventStartDate + "T" + this.eventStartTime
-        : this.eventStartDate;
-      let end = this.eventEndTime
-        ? this.eventEndDate + "T" + this.eventEndTime
-        : this.eventEndDate;
+      let start = this.newEvent.eventStartTime
+        ? this.newEvent.eventStartDate + "T" + this.newEvent.eventStartTime
+        : this.newEvent.eventStartDate;
+      let end = this.newEvent.eventEndTime
+        ? this.newEvent.eventEndDate + "T" + this.newEvent.eventEndTime
+        : this.newEvent.eventEndDate;
 
       let newCalendarEvent = {
-        title: this.eventTitle,
+        title: this.newEvent.eventTitle,
         start,
         end,
-        allDay: this.eventAllDay,
+        allDay: this.newEvent.eventAllDay,
         userId: this.$store.getters.getUser.id
       };
-
-     
-
-      let url = process.env.VUE_APP_HOST + ":" + process.env.VUE_APP_SERVER_PORT + "/";
+      let url =
+        process.env.VUE_APP_HOST + ":" + process.env.VUE_APP_SERVER_PORT + "/";
 
       this.axios
         .post(url + "calendar/addEvent", newCalendarEvent)
@@ -134,6 +166,25 @@ export default {
     },
     removeAddEventPopup() {
       this.statusAddEventPopup = false;
+    },
+
+    eventClick(info) {
+      let url =
+        process.env.VUE_APP_HOST + ":" + process.env.VUE_APP_SERVER_PORT + "/";
+
+      this.axios
+        .post(url + "calendar/getEvent", { id: info.event.id })
+        .then(res => {
+          this.eventInfo = res.data[0];
+          console.log(res);
+        })
+        .catch(err => {
+          console.log(err);
+          this.errorMessage = err.response.data.msg;
+        });
+    },
+    removeEventInfoPopup() {
+      this.eventInfo = null;
     }
   }
 };
@@ -183,11 +234,43 @@ export default {
   box-sizing: border-box;
 }
 
+.selectWrapper {
+  display: inline-flex;
+  margin: 5px;
+}
+
 .checkboxWrapper {
   display: inline-flex;
 }
 
 .dateTimeBox {
+  display: inline-flex;
+}
+
+.showEventInfoPopup {
+  position: fixed;
+  background: white;
+  padding: 15px;
+  top: 50%;
+  left: 45%;
+  z-index: 15;
+  border-radius: 6px;
+  -webkit-box-shadow: 0px 0px 9px -5px rgba(138, 138, 138, 1);
+  -moz-box-shadow: 0px 0px 9px -5px rgba(138, 138, 138, 1);
+  box-shadow: 0px 0px 9px -5px rgba(138, 138, 138, 1);
+}
+
+.showEventInfoPopup form {
+  display: grid;
+}
+
+.showEventInfoPopup input {
+  display: block;
+  padding: 5px;
+  margin: 5px;
+  box-sizing: border-box;
+}
+.showTimeBox {
   display: inline-flex;
 }
 </style>
