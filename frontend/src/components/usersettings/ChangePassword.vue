092dfb14f78d6e52b2db233cfcfeb0bcbb35a17f
NicklasPekkarinen <56152106+NicklasPekkarinen@users.noreply.github.com>
2020-06-15 13:13:46 +0200

    Prettyfied user settings page (#46)

--- frontend/src/components/usersettings/ChangePassword.vue
+++ frontend/src/components/usersettings/ChangePassword.vue
@@ -3,9 +3,9 @@
     <h2>Ändra lösenord</h2>
     <div class="reset-password">
       <form autocomplete="off">
-        <input type="password" v-model="oldPass" placeholder="Nuvarande lösenord" />
-        <input type="password" v-model="newPass" placeholder="Nytt lösenord" />
-        <input type="password" v-model="repeatedPass" placeholder="Upprepa lösenord" />
+        <input type="password" v-model="oldPass" placeholder="Nuvarande lösenord" required />
+        <input type="password" v-model="newPass" placeholder="Nytt lösenord" required />
+        <input type="password" v-model="repeatedPass" placeholder="Upprepa lösenord" required />
         <button @click="savePassword()">Spara</button>
       </form>
     </div>
@@ -48,4 +48,41 @@ export default {
 </script>
 
 <style scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 0 auto;
+}
+h2 {
+  font-family: "Spartan", sans-serif;
+  font-weight: 500;
+  margin: 20px 0 10px 0;
+}
+form input {
+  display: block;
+  height: 20px;
+  width: 10vw;
+  padding: 10px;
+  margin: 10px 0;
+  border-radius: 15px;
+  border: none;
+}
+form button {
+  box-shadow: inset 0px 1px 0px 0px #ffffff;
+  background-color: #ffffff;
+  opacity: 0.9;
+  border-radius: 15px;
+  border: none;
+  display: inline-block;
+  cursor: pointer;
+  font-family: Arial;
+  font-size: 0.9em;
+  padding: 6px 15px;
+  margin-bottom: 20px;
+}
+form button:hover {
+  opacity: 1;
+}
 </style>
